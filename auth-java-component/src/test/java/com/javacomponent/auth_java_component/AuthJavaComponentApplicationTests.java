f89240edc7c9719f6f18c5ba9232342e916c40a5
JulianHuertasUNAL <jhuertasd@unal.edu.co>
2025-05-14 21:03:27 -0500

    feature:  Se añade microservicio básico de SpringBoot para las funcionalidades de autenticación.

---
+++ auth-java-component/src/test/java/com/javacomponent/auth_java_component/AuthJavaComponentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javacomponent.auth_java_component;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthJavaComponentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
